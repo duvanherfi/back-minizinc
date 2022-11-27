831252e58058b34a59d2b9c15d44e945d300d231
Duvan Hernandez Figueroa <duvanherfi@gmail.com>
2022-11-26 20:02:06 -0500

    Add body in solve

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
-from fastapi import FastAPI
-from minizinc import Instance, Model, Solver
+from minizinc import Instance, Model, Solver, error
+
+
+from typing import Union
+
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class Data(BaseModel):
+    n:int
+    paginas:int
+    pag_min:list
+    pag_max:list
+    lectores:list
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
 
-@app.get("/solve")
-def solve():
+@app.post("/solve")
+def solve(data: Data = Body(embed=True)):
     print("HOla")
-    gecode = Solver.lookup("gecode")
+    print(data)
+    solver = Solver.lookup("chuffed")
 
     model = Model()
     model.add_string(
         """
         int: n;
         int: paginas;
-        var int:suma;
         
         array[1..n] of int: pagMin;
         array[1..n] of int: pagMax;
@@ -29,25 +41,19 @@ def solve():
         constraint forall(i in 1..n) (cantidad[i] >= 0 \/ cantidad[i] >= pagMin[i]);
         constraint forall(j in 1..n) (cantidad[j] <= pagMax[j]);
         constraint sum(k in 1..n) (cantidad[k]) = paginas;
-        constraint suma = sum(l in 1..n) (cantidad[l] * lectores[l]);
-        solve maximize suma;
+        solve maximize sum(l in 1..n) (cantidad[l] * lectores[l]);
         """
     )
-    n = 5;
-    paginas = 10;
 
-    pagMin = [5, 4, 2, 2, 1];
-    pagMax = [9, 7, 5, 4, 3];
-    lectores = [1500, 2000, 1000, 1500, 750];
+    instance = Instance(solver, model)
+    instance["n"] = data.n
+    instance["paginas"] = data.paginas
+    instance["pagMin"] = data.pag_min
+    instance["pagMax"] = data.pag_max
+    instance["lectores"] = data.lectores
 
-    instance = Instance(gecode, model)
-    instance["n"] = n
-    instance["paginas"] = paginas
-    instance["pagMin"] = pagMin
-    instance["pagMax"] = pagMax
-    instance["lectores"] = lectores
-
-    print(instance)
-    result = instance.solve(all_solutions=True)
+    print(instance["paginas"])
+    result = instance.solve()
     print(result)
-    return {'datos': result["datos"]}
+    return {'cantidad': result["cantidad"], 'lectores': result["objective"]}
+
