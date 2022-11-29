7fc397a665e7cb03ee931c520467481724786925
Duvan Hernandez <dhernandez@pibox.app>
2022-11-29 10:26:59 -0500

    Add cores

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from minizinc import Instance, Model, Solver, error
-
-
-from typing import Union
-
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Data(BaseModel):
     n:int
